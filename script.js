999d2538e89d924720060943a0f2068e3ccadf67
michaelmargiotta97-ops <michael.margiotta97@gmail.com>
2025-11-23 07:42:12 +0000

    Add files via upload
    Script.js for the beer printing and the changes to rock paper scissors

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
 console.log("script.js loaded");
 
 
+//counter for playing rock paper scissors 3 times
 
 
-function RockPaperScissors(a,b){
-	if ( a == b){
-		alert("it is a draw");
+function RockPaperScissors(){
+	var counter = 0;
+	while(counter < 3){
+			let a = prompt("Player A: Rock, Paper, or Scissors?"); //user input everytime
+        	let b = prompt("Player B: Rock, Paper, or Scissors?");
+
+
+			if ( a == b){
+			alert("it is a draw");
+			counter++;
+		}
+		else if( a == "Rock" && b == "Scissors" || a == "Scissors" && b == "Paper" || a == "Paper" && b == "Rock" ) {
+			alert("Player A wins");
+			counter++;
+		}
+		else{
+			alert("Player B wins");
+			counter++;
+		}
+
 	}
-	else if( a == "Rock" && b == "Scissors" || a == "Scissors" && b == "Paper" || a == "Paper" && b == "Rock" ) {
-		alert("Player A wins");
+	alert("you have played 3 times, the game is now over")
+}
+//prints beer lyrics
+function printBeer(){
+	let beer = 0;
+	let message = " "
+	for(let i = 99; i >= beer; i--){
+		message += i + " bottles of beer on the wall " + i + " bottles of beer <br> ";
+		message += " take one down and pass it around " + i + " bottles of beer on the wall <br>";
 	}
-	else{
-		alert("Player B wins");
-	}
+	message+="No more bottles of beer on the wall, no more bottles of beer.<br>";
+	message+="Go to the store and buy some more, 99 bottles of beer on the wall.";
+
+	document.getElementById("button").innerHTML = message;
 }
